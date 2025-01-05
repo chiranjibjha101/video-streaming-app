579e0c5b9b017d40872b7fb16cc3cfe35fc6e36a
chiranjibJha <chiranjibjha1999@gmail.com>
2025-01-05 22:26:38 +0530

    cors added

--- src/app.ts
+++ src/app.ts
@@ -1,17 +1,18 @@
 import express from "express";
 import dotenv from "dotenv";
 // import connectDB from "./config/database";
+import cors from "cors";
 import authRoutes from "./routes/auth.routes";
 import testRoutes from "./routes/test.routes";
 
 dotenv.config();
 
 const app = express();
-
+app.use(cors());
 // Middleware
 app.use(express.json());
 
-// Connect to DB
+// Connect to DBS
 
 // (async function () {
 //   await connectDB();
